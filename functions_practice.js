1a6b2214cdc40167421a4e085afb5d7d9880aa41
Aria Nikzad <a.c.nikzad@gmail.com>
2022-05-18 18:54:12 -0700

    did some more js exercises

--- functions_practice.js
+++ functions_practice.js
@@ -3,7 +3,6 @@
 function timesTwo(num) {
   return num * 2
 }
-
 console.log(timesTwo(2))
 
 // 2. Write a function that takes in a string and returns the string with all capital letters. Then run the function and print the result.
@@ -11,35 +10,62 @@ console.log(timesTwo(2))
 function capitalize(string) {
   return string.toUpperCase();
 }
-
 console.log(capitalize("Lets get this interview!"))
 
-// 3. Write a function that takes in two numbers and returns the first number subtracted by the second. Then run the function and print the result.
+// 3. Write a function that takes in two numbers and returns the first number subtracted by the scond. Then run the function and print the result.
 
 function difference(num1, num2) {
   return num1 - num2;
 }
-
 console.log(difference(5, 6))
 
 // 4. Write a function that takes in a number and returns the number times itself. Then run the function and print the result.
 
+function multiply(num) {
+  return num*num
+}
+console.log(multiply(5))
 
 // 5. Write a function that takes in a string and returns the first letter of the string. Then run the function and print the result.
 
+function first(string) {
+  return string[0];
+}
+console.log(first("hello"))
 
 // 6. Write a function that takes in three strings and returns a string that combines all three strings with spaces in between. Then run the function and print the result.
 
+function space(string1, string2, string3){
+  return string1 + " " + string2 + " " + string3
+}
+console.log("hello", "world", "leggo")
 
 // 7. Write a function that takes in a number and returns the number as a string. Then run the function and print the result.
 
+function numtostring(num) {
+  return num.toString();
+}
+console.log(numtostring(3))
 
 // 8. Write a function that takes in a string and returns the string repeated 5 times. Then run the function and print the result.
 
+function five(string) {
+ return string + string + string + string + string
+}
+console.log(five("fiftyfive"))
 
 // 9. Write a function that takes in 3 numbers and returns the average (the sum divided by 3.0). Then run the function and print the result.
 
+function average(num1, num2, num3) {
+  return (num1 + num2 + num3)/3
+}
+console.log(average(4,90,32))
 
 // 10. Write a function that takes in a number and returns the number times 10 plus 30. Then run the function and print the result.
+
+function raulito(num1) {
+  return (num1*10)+30
+}
+console.log(raulito(3.423))
 
 // SOLUTIONS: https://gist.github.com/peterxjang/1c79bc30b1e377a1852511b619259f79
